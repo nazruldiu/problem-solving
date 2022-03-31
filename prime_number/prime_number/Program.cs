56e67dc29452697aa27389a893a8578bd937be78
nazruldiu <nazruldiu26@gmail.com>
2022-03-31 06:12:15 +0600

    Ugly Number and Prime number

---
+++ prime_number/prime_number/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("End number: ");
+            var endNumber = Convert.ToInt32(Console.ReadLine());
+
+            var s = new Solution();
+            //var IsPrime = s.IsPrime(number);
+            //Console.WriteLine("Number is prime: {0}", IsPrime);
+
+            s.PrimeNumber(number, endNumber);
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPrime(int n)
+        {
+            bool r = true;
+            for(int i= 2; i<n  ; i++)
+            {
+                if (n % i == 0)
+                {
+                    r = false;
+                    break;
+                }
+            }
+            return r;
+        }
+
+        public bool PrimeRecursion(int n, int i)
+        {
+            if(i>=2 && n > i)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            if(i>n)
+            {
+                return true;
+            }
+
+            return PrimeRecursion(n, i+1);
+        }
+
+        public void PrimeNumber(int x, int y)
+        {
+            for (int i = x; i <= y; i++)
+            {
+                if (PrimeRecursion(i, 2) == true && i > 1)
+                    //if(IsPrime(i) == true && i > 1)
+                    Console.WriteLine("Prime number: {0}", i);
+            }
+        }
+    }
+}
---
+++ ugly-number/ugly-number/ugly-number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ugly_number
+{
+    class Program
+    {
+        /*
+         * Description
+          An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+          Given an integer n, return true if n is an ugly number. 
+        */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a iteger: ");
+            var x=   Console.ReadLine();
+
+            var s = new Solution();
+            var result = s.get( Convert.ToInt32(x));
+            Console.WriteLine(result);
+        }  
+    }
+
+    public class Solution
+    {
+        public bool get(int n)
+        {
+            if (n > 0)
+            {
+                for (; n % 2 == 0; n /= 2);
+                for (; n % 3 == 0; n /= 3) ;
+                for (; n % 5 == 0; n /= 5) ;
+            }
+            return n == 1;
+        }
+    }
+}
