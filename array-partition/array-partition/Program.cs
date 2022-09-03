c7c94103771e00669491dad3cfd4b879a6cb008a
Nazrul Islam <nazruldiu26@gmail.com>
2022-09-03 11:17:00 +0600

    Fixed sorting

--- array-partition/array-partition/Program.cs
+++ array-partition/array-partition/Program.cs
@@ -15,8 +15,8 @@ namespace array_partition
             int[] num = { 1, 4, 3, 2, 5 };
 
             //Console.WriteLine(sum(num));
-            //Sort(num);
-            LinearSearch(num, 3);
+            Sort(num);
+            //LinearSearch(num, 3);
         }
 
         public static int sum(int[] number)
@@ -32,15 +32,14 @@ namespace array_partition
 
         public static void Sort(int[] number)
         {
-            int temp;
             for (int i = 0; i < number.Length; i++)
             {
-                temp = number[i];
-                for (int j = i+1; j < number.Length; j++)
+                for (int j = 0; j < number.Length - 1; j++)
                 {
-                    if (number[i] > number[j])
+                    if (number[j] > number[j + 1])
                     {
-                        number[i] = number[j];
+                        int temp = number[j + 1];
+                        number[j + 1] = number[j];
                         number[j] = temp;
                     }
                 }
