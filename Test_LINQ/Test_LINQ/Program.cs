afd3a1c013a732b70ba51aaba2c3cf492bbbb0c5
Nazrul Islam <nazrul.islam@technovista.com.bd>
2023-02-09 18:24:01 +0600

    Duplicate String

---
+++ Test_LINQ/Test_LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Test_LINQ;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string myString = "helloo";
+
+        char[] arr = myString.ToCharArray();
+
+        var result = arr.GroupBy(x => x)
+                    .Where(x => x.Count() > 1)
+                    .Select(x => x.Key);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadLine();
+    }
+}
