60ee36a9cf0a7f57bf558f4a2728c0686b20acfa
Nazrul Islam <nazrul.islam@technovista.com.bd>
2023-02-14 11:15:03 +0600

    Single delegate

--- Test_LINQ/Test_LINQ/Program.cs
+++ Test_LINQ/Test_LINQ/Program.cs
@@ -2,6 +2,9 @@
 
 internal class Program
 {
+    public delegate int Calculate(int firstNumber, int secondNumber);
+
+
     static void Main(string[] args)
     {
         string myString = "helloo";
@@ -11,10 +14,26 @@ internal class Program
         var result = arr.GroupBy(x => x)
                     .Where(x => x.Count() > 1)
                     .Select(x => x.Key);
-        foreach (var item in result)
-        {
-            Console.WriteLine(item);
-        }
+        //foreach (var item in result)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        /* To point or initialize a method to a delegate signature must need to be equal 
+         * that means (a) delegate return type and method must be same and (b) parameters need to be same also.
+         */
+        //Calculate calculate = new Calculate(Sum);  // we can point delegate in this way.
+        Calculate calculate = Sum;                   // we can point delegate in this way too.
+
+        //int myResult = calculate.Invoke(5, 5);    // we can invoke delegate in this way.
+        int myResult = calculate(5, 5);             // we can invoke delegate in this way too.
+
+        Console.WriteLine(myResult);
         Console.ReadLine();
+    }
+
+    static int Sum(int x, int y)
+    {
+        return x + y;
     }
 }
