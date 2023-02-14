29f5f99279fd4d67753ca36b654ad9b3c0f109be
Nazrul Islam <nazrul.islam@technovista.com.bd>
2023-02-14 12:05:05 +0600

    method as a parameter using C# delegate

--- Test_LINQ/Test_LINQ/Program.cs
+++ Test_LINQ/Test_LINQ/Program.cs
@@ -22,18 +22,35 @@ internal class Program
         /* To point or initialize a method to a delegate signature must need to be equal 
          * that means (a) delegate return type and method must be same and (b) parameters need to be same also.
          */
-        //Calculate calculate = new Calculate(Sum);  // we can point delegate in this way.
+        //Calculate calculate = new Calculate(Sum);  // we can point delegate in this way. Here 'calculate' is pointer or reference
         Calculate calculate = Sum;                   // we can point delegate in this way too.
 
         //int myResult = calculate.Invoke(5, 5);    // we can invoke delegate in this way.
-        int myResult = calculate(5, 5);             // we can invoke delegate in this way too.
+        //int myResult = calculate(5, 5);           // we can invoke delegate in this way too.
 
-        Console.WriteLine(myResult);
+        //Console.WriteLine(myResult);
+
+        MyDelegateMethod(calculate,5,5);   // Passing method as a parameter using C# delegate.
+
+        Calculate calculate1 = Sub;
+
+        MyDelegateMethod(calculate1, 5, 5); // Passing method as a parameter using C# delegate.
+
         Console.ReadLine();
     }
 
     static int Sum(int x, int y)
     {
         return x + y;
+    }
+
+    static int Sub(int x, int y)
+    {
+        return x - y;
+    }
+
+    static void MyDelegateMethod(Calculate calculate, int fNumber, int sNumber)
+    {
+        Console.WriteLine(calculate(fNumber, sNumber));
     }
 }
