eaf8c1ce99224418cb7b15619896812e476fb614
Nazrul Islam <nazrul.islam@technovista.com.bd>
2023-03-09 11:16:40 +0600

    Sorting and removing duplicate

---
+++ LinkedInApp/LinkedInApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LinkedInApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myString = new List<string> {"Dhaka", "Comilla", "Dhaka", "Mymensing", "Barishal" };
+
+            //// Order By
+            //myString.Sort();
+
+            //myString.Reverse();
+
+            //myString.OrderBy(x => x);
+
+            //myString.OrderByDescending(x => x);
+
+            //// removing Duplicate
+            //var uniqueString = myString.Distinct().ToList();
+            //var uniqueString = myString.ToHashSet().ToList();
+            //var uniqueString = myString.GroupBy(x=>x).Select(d=>d.First()).ToList();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
