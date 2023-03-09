d0355df0b5f01609ef1f1fc06212138f7d6d1cb6
Nazrul Islam <nazrul.islam@technovista.com.bd>
2023-03-09 12:53:37 +0600

    OOP

--- LinkedInApp/LinkedInApp/Program.cs
+++ LinkedInApp/LinkedInApp/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace LinkedInApp
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LinkedInApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<string> myString = new List<string> {"Dhaka", "Comilla", "Dhaka", "Mymensing", "Barishal" };
+            List<string> myString = new List<string> { "Dhaka", "Comilla", "Dhaka", "Mymensing", "Barishal" };
 
             //// Order By
             //myString.Sort();
@@ -20,7 +22,41 @@
             //var uniqueString = myString.ToHashSet().ToList();
             //var uniqueString = myString.GroupBy(x=>x).Select(d=>d.First()).ToList();
 
-            Console.WriteLine("Hello, World!");
+
+            MyBase myBase = new MyDerived();
+            myBase.Install();
+            //GetName(myBase);
+
+            //Console.WriteLine(myBase.Name);
+        }
+
+        //public static void GetName(MyDerived my){
+        //    Console.WriteLine(my.Name);
+        //}
+
+    }
+
+    public class MyBase
+    {
+        public void Install()
+        {
+            Console.WriteLine("Installed");
+        }
+        public virtual void Print( string text)
+        {
+            Console.WriteLine("Print :"+ text);
+        }
+    }
+
+    public class MyDerived : MyBase
+    {
+        public new void Install()
+        {
+            Console.WriteLine("MyDerived Installed");
+        }
+        public override void Print(string text)
+        {
+            Console.WriteLine("MyDerived Print :" + text);
         }
     }
 }
